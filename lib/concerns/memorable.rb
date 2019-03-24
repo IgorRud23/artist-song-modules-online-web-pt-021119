eabf6041ddd727896d5d2316783b3daeb388fc0a
Igor Rud <rud.igor23@gmail.com>
2019-03-24 15:59:55 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,6 @@
 module Memorable
 
+  module ClassMethods
   def reset_all
     self.all.clear
   end
@@ -7,5 +8,9 @@ module Memorable
   def count
     self.all.count
   end
+end 
+
+  module InstanceMethods
+
 
 end
